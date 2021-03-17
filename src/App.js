782fa933998f13b15db7d58d9336df5a1b9149b2
Jennifer Martelle <jennifermartelle@Jennifers-MBP.home>
2021-03-17 17:01:14 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="intro">Hello, I'm Jen Stella.</h1>
+      <h1 className="subIntro">I'm a Full-Stack Developer<br></br>specializing in Front-End work.</h1>
     </div>
   );
 }
