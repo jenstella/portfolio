981e145e1f1f26778445156041bb6a43fa0e4c96
Jennifer Martelle <jennifermartelle@Jennifers-MBP.home>
2021-03-18 13:25:17 -0400

    going to heroku

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
       </h1>
       <img
         id="photo-of-me"
-        src="https://scontent-bos3-1.xx.fbcdn.net/v/t1.0-9/43551157_2185515355041226_2924433817278087168_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=nbvgp0i6OHYAX_M2m1n&_nc_ht=scontent-bos3-1.xx&oh=7f09a504cf7cd717bdbcab3a9656ff4f&oe=6078C473"
+        src="https://i.pinimg.com/564x/54/6c/ce/546ccef1e8be5e15e1cfec197d7cb5c5.jpg"
         alt="jen sitting at her laptop"
       ></img>
     </div>
