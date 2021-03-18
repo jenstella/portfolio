44c1338276c7fe8a80fd51517e03b380f2dc3592
Jennifer Martelle <jennifermartelle@Jennifers-MBP.home>
2021-03-17 20:46:30 -0400

    weds night

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import './Styles/index.css';
 
 function App() {
   return (
     <div className="App">
       <h1 className="intro">Hello, I'm Jen Stella.</h1>
-      <h1 className="subIntro">I'm a Full-Stack Developer<br></br>specializing in Front-End work.</h1>
+      <h1 className="subIntro">I'm a Full-Stack Developer<br></br>specializing in front end work.</h1>
     </div>
   );
 }
