e6e3bd8f344d62bad86fbebc736feb22a5607c48
Jennifer Martelle <jennifermartelle@Jennifers-MBP.home>
2021-03-18 11:47:16 -0400

    thursday updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import "./Styles/index.css";
-import SocialLinks from './components/SocialLinks'
-import Home from './components/Home'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-// import About from "./About";
-
+import SocialLinks from "./components/SocialLinks";
+import Home from "./components/Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter id="browser">
-        <Switch >
-        <Route
-              //homepage
-              exact
-              path="/"
-              render={() => {
-                return <Home />;
-              }}
-            />        
-            </Switch>
+        <Switch>
+          <Route
+            //homepage
+            exact
+            path="/"
+            render={() => {
+              return <Home />;
+            }}
+          />
+          <Route
+            path="/about"
+            render={() => {
+              return <About />;
+            }}
+          />
+        </Switch>
         <SocialLinks />
-
       </BrowserRouter>
     </div>
   );
