176b99c2c667d9088371ad0d3469d0eae507d54d
Jennifer Martelle <jennifermartelle@Jennifers-MBP.home>
2021-03-19 10:10:21 -0400

    footer fixed

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,8 @@ export default function About() {
       </div>
       <div id="hobbies-container">
           <h3 className="hobbies-header">Hobbies & Interests</h3>
-          <p>I love to travel (when the world is not in a pandemic). I spend as much time as I can outside, with my 2 dogs and my family. We love to snowboard, hike, and hang by a nice river in the summer.</p>
+          <p>I love to travel (when the world is not in a pandemic). I spend as much time as I can outside, with my 2 dogs and my family. We love to snowboard, hike, and hang by a nice river in the summer. 
+          </p>
       </div>
       <h1 id="about-two">&lt;/about&gt;</h1>
     </div>
