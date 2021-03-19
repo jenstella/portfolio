6e71251ee976725785d80e27af6b6b35a6a56bde
Jennifer Martelle <jennifermartelle@Jennifers-MBP.home>
2021-03-19 16:53:26 -0400

    line breaks in projs

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -29,7 +29,7 @@ export default function Projects() {
           github repo
         </a>
         <img src={til} alt="screenshot of today I learned" />
-
+        <hr></hr>
 
         <h1 className="proj-title">Yelpington</h1>
         <h3>project description</h3>
@@ -47,7 +47,7 @@ export default function Projects() {
           github repo
         </a>
         <img src={yelpington} alt="screenshot of yelpington" />
-
+        <hr></hr>
 
         <h1 className="proj-title">Geo-Vermonter</h1>
         <h3>project description</h3>
@@ -66,7 +66,7 @@ export default function Projects() {
         </a>
         <br></br>
         <img src={geoVermonter} alt="screenshot of geovermonter" />
-
+        <hr></hr>
 
         <h1 className="proj-title">Tic Tac Toe</h1>
         <h3>project description</h3>
@@ -85,7 +85,7 @@ export default function Projects() {
         </a>
         <br></br>
         <img src={ttt} alt="screenshot of tic tac toe" />
-
+        <hr></hr>
 
         <h1 className="proj-title">Remock</h1>
         <h3>project description</h3>
@@ -104,8 +104,7 @@ export default function Projects() {
         </a>
         <br></br>
         <img src={remock} alt="screenshot of remock" />
-
-
+        <hr></hr>
 
         <h1 className="proj-title">Zorkington</h1>
         <h3>project description</h3>
@@ -124,7 +123,7 @@ export default function Projects() {
         </a>
         <br></br>
         <img src={zorkington} alt="screenshot of zorkington" />
-
+        <hr></hr>
 
         <h1 className="proj-title">Guess The Number</h1>
         <h3>project description</h3>
@@ -144,7 +143,6 @@ export default function Projects() {
         <br></br>
         <img src={guess} alt="screenshot of guess the number" />
       </div>
-
 
       <h1 id="projects-two">&lt;/projects&gt;</h1>
     </div>
