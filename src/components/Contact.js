1ef481b343c00deb8e9c96aee89485215e436f54
Jennifer Martelle <jennifermartelle@Jennifers-MBP.home>
2021-03-18 21:10:01 -0400

    updated fonts and pages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function Contact() {
-    return (
-        <div id="contact-info">
-            <div id="contact-left">
-                <h2>Email: jennifer.stella.martelle@gmail.com</h2>
-                <br></br>
-                <h2>Phone: 802-373-9851</h2>
-            </div>
-            <div id="contact-right">
-                <h2>Linkedin: linkedin.com/in/jennifermartelle</h2>
-                <h2>Github: github.com/jenstella</h2>
-            </div>
-        </div>
-    )
+  return (
+    <div id="contact-info">
+      <h1 id="contact-one">&lt;contact&gt;</h1>
+      <div id="contact-container">
+        <h2>Email: jennifer.stella.martelle@gmail.com</h2>
+        <h2>Phone: 802-373-9851</h2>
+        <h2>Linkedin: linkedin.com/in/jennifermartelle</h2>
+        <h2>Github: github.com/jenstella</h2>
+      </div>
+      <h1 id="contact-two">&lt;/contact&gt;</h1>
+    </div>
+  );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Projects() {
     return (
-        <div>
-            
+        <div id="projects-container">
+        <h1 id="projects-one">&lt;projects&gt;</h1>
+        <div id="projects-container">
+            Some shit I built!
         </div>
+        <h1 id="projects-two">&lt;/projects&gt;</h1>
+      </div>
     )
 }
